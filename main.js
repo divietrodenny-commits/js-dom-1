3dfad3124f4d78712ba690e88f56ce398c57a1b3
divietrodenny-commits <divietrodenny@gmail.com>
2026-02-12 19:19:09 +0100

    aggiunti se ed ltrimenti per far accendere e spegnere la lampadina

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ const bottone = document.getElementById('interruttore');
 /* Aggiungo l'ascoltatore di eventi sul click del bottone */
 bottone.addEventListener('click', function () {
 
-  /* quando clicco, cambio solo l'immagine da spenta ad accesa */
+    /* se contiene spenta la accendo */
+   if (lampadina.src.includes('white_lamp.png')) {
   lampadina.src = './img/yellow_lamp.png';
-});
+  bottone.textContent = 'Spegni';
+    } 
+
+    /* altrimenti la spengo */
+else {
+    lampadina.src= './img/white_lamp.png';
+    bottone.textContent= 'Accendi';
+}
+})
+
+/* quando clicco, cambio solo l'immagine da spenta ad accesa 
+lampadina.src = './img/yellow_lamp.png'; */
